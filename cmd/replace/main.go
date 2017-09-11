86b385c3643b24a4280c4e70d73fca173239a205
Ray Pereda <raypereda@gmail.com>
2017-09-10 17:05:09 -1000

    Add regex pattern match; not working yet

--- cmd/replace/main.go
+++ cmd/replace/main.go
@@ -1,26 +1,66 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
 	"flag"
+	"fmt"
+	"log"
+	"os"
 	"path/filepath"
+	"regexp"
 )
 
-
-var pattern = flag.String("pattern", "", "line pattern for searching for")
+var patternFlag = flag.String("pattern", "", "line pattern for searching for")
 var rline = flag.String("rline", "", "replace line for matched lines")
 var inGlob = flag.String("inglob", "", "the glob pattern of input text files")
+var pattern = regexp.MustCompile(*patternFlag)
 
 func main() {
 	flag.Parse()
-	fmt.Println("pattern:", *pattern)
+	fmt.Println("pattern:", *patternFlag)
 	fmt.Println("rline: ", *rline)
+	fmt.Println("pattern: ", pattern)
+
+	editFiles()
+	fmt.Println("hello")
+}
+
+func editFiles() {
 	infiles, err := filepath.Glob(*inGlob)
 	if err != nil {
 		panic("not value Glob pattern")
 	}
-
 	fmt.Println("infiles: ", infiles)
 
-	fmt.Println("hello")
+	for _, filename := range infiles {
+		fmt.Println("editing file: ", filename)
+		editFile(filename)
+	}
+}
+
+func editFile(filename string) {
+	// open a file
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+
+	// make sure it gets closed
+	defer file.Close()
+
+	// create a new scanner and read the file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if pattern.MatchString(line) {
+			fmt.Print("MATCH: ")
+		}
+		fmt.Println(line)
+	}
+
+	// check for errors
+	if err = scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
