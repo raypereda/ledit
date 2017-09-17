5ed29fa954f57e7acb165a154435b1d64155b78a
Ray Pereda <raypereda@gmail.com>
2017-09-17 15:55:35 -0700

    Small flag changes

--- cmd/replace/main.go
+++ cmd/replace/main.go
@@ -11,52 +11,78 @@ import (
 )
 
 var patternFlag = flag.String("pattern", "", "line pattern for searching for")
-var rline = flag.String("rline", "", "replace line for matched lines")
-var inGlob = flag.String("inglob", "", "the glob pattern of input text files")
 var pattern = regexp.MustCompile(*patternFlag)
+var replace = flag.String("replace", "", "replace line for matched lines")
+var inFiles = flag.String("infiles", "", "the glob pattern of input text files")
+
+// var flag.Usage = func() {
+//     fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+//     fmt.Fprintf(os.Stderr, "Replace is a tool for replacing lines of text.
+
+// }
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Replace is a tool for replacing lines in text files.\n")
+		fmt.Fprintf(os.Stderr, "Lines matching a regular expression are replaced with fixed text.\n")
+		flag.PrintDefaults()
+	}
+	
 	flag.Parse()
-	fmt.Println("pattern:", *patternFlag)
-	fmt.Println("rline: ", *rline)
-	fmt.Println("pattern: ", pattern)
-
+	pattern = regexp.MustCompile(*patternFlag)	
+	fmt.Println("pattern: \"", pattern, "\"")	
+	fmt.Println("replace: \"", *replace, "\"")
+	fmt.Println("infiles: \"", *inFiles, "\"")
 	editFiles()
-	fmt.Println("hello")
 }
 
 func editFiles() {
-	infiles, err := filepath.Glob(*inGlob)
+	inFiles, err := filepath.Glob(*inFiles)
 	if err != nil {
-		panic("not value Glob pattern")
+		panic("not valid glob pattern of input text files")
 	}
-	fmt.Println("infiles: ", infiles)
+	fmt.Println("infiles: ", inFiles)
 
-	for _, filename := range infiles {
+	for _, filename := range inFiles {
 		fmt.Println("editing file: ", filename)
 		editFile(filename)
 	}
 }
 
 func editFile(filename string) {
-	// open a file
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 		panic(err)
 	}
-
-	// make sure it gets closed
 	defer file.Close()
 
+	outFile, err := os.Create(filename + ".edited")
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+	defer outFile.Close()
+
+	lineNum := 1
 	// create a new scanner and read the file line by line
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
+		outLine := line
 		if pattern.MatchString(line) {
-			fmt.Print("MATCH: ")
+			fmt.Printf("line: %3d", lineNum) 
+			fmt.Println(" matched: ", line)
+			
+			outLine = *replace
+			fmt.Println("replaced: ", outLine)	
 		}
-		fmt.Println(line)
+		_, err := fmt.Fprintln(outFile, outLine)
+		if err != nil {
+			panic(err)
+		}
+		lineNum++
 	}
 
 	// check for errors
